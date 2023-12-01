0ca1f2a726deffc470c8fa8764bb935c08a23e75
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2023-12-01 10:50:48 +0000

    Day1, P2

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,7 +1,7 @@
 
 #[aoc::main(1)]
 fn main(input: &str) -> (usize, usize) {
-    let vals = input
+    let mut vals = input
         .lines()
         .map(|line| {
             let t = line.chars()
@@ -11,5 +11,28 @@ fn main(input: &str) -> (usize, usize) {
         })
         .collect::<Vec<String>>();
 
-    (vals.iter().map(|s| s.parse::<u32>().unwrap()).sum::<u32>() as usize, 0)
+    let p1 = vals.iter().map(|s| s.parse::<u32>().unwrap()).sum::<u32>() as usize;
+
+    vals = input
+        .lines()
+        .map(|line| {
+            let mut new_line = line.replace("one", "o1e");
+            new_line = new_line.replace("two", "t2o");
+            new_line = new_line.replace("three", "t3e");
+            new_line = new_line.replace("four", "f4r");
+            new_line = new_line.replace("five", "f5e");
+            new_line = new_line.replace("six", "s6x");
+            new_line = new_line.replace("seven", "s7n");
+            new_line = new_line.replace("eight", "e8t");
+            new_line = new_line.replace("nine", "n9e");
+            let t = new_line.chars()
+                .filter(|d| *d >= '0' && *d <= '9')
+                .collect::<String>();
+            format!("{}{}", t.chars().next().unwrap(), t.chars().last().unwrap())
+        })
+        .collect::<Vec<String>>();
+
+    let p2 = vals.iter().map(|s| s.parse::<u32>().unwrap()).sum::<u32>() as usize;
+
+    (p1 as usize, p2 as usize)
 }
