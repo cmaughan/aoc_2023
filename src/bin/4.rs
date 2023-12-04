c5bffd3ff72672603864c5b3cdc32b0a7b7fe404
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2023-12-04 12:08:28 +0000

    Cleaner

--- src/bin/4.rs
+++ src/bin/4.rs
@@ -1,40 +1,35 @@
-use std::collections::HashSet;
-
 #[aoc::main(4)]
 fn main(input: &str) -> (usize, usize) {
-
     let mut tot_all = 0;
-    let mut mult =  vec![1 as usize; input.lines().count()];
+    let mut mult = vec![1 as usize; input.lines().count()];
+    let mut nums = vec![0; 100];
     for (index, line) in input.lines().enumerate() {
 
-        let c = line.split("|").collect::<Vec<_>>();
-        let card_digits = c[0].trim().split(":").nth(1).unwrap();
-        let card = card_digits
-            .trim()
-            .split(|c| c == ' ' || c == ':')
-            .filter(|c| !c.is_empty())
-            .collect::<Vec<_>>();
-        let rem = c[1]
-            .trim()
-            .split(|c| c == ' ' || c == ':')
-            .filter(|c| !c.is_empty())
-            .collect::<HashSet<_>>();
+        nums.iter_mut().for_each(|a| *a = 0);
 
-        let mut tot = 0;
-        for c in card {
-            if rem.contains(c) {
-                tot += 1;
-            }
-        }
-            
-        let current = mult[index];
+        let card_matches = line.split("|").collect::<Vec<_>>();
+
+        let card = card_matches[0].trim().split(":").nth(1).unwrap();
+
+        card_matches[1]
+            .trim()
+            .split(|c| c == ' ' || c == ':')
+            .filter_map(|c| c.parse::<u32>().ok())
+            .for_each(|m| nums[m as usize] = 1 );
+
+        let tot = card
+            .trim()
+            .split(|c| c == ' ' || c == ':')
+            .filter_map(|c| c.parse::<u32>().ok())
+            .filter(|v| nums[*v as usize] != 0)
+            .count();
+
         if tot > 0 {
             tot_all += 1 << (tot - 1);
 
-            for _ in 0..current {
-                for slot in (index + 1)..(index + 1 + tot) {
-                    mult[slot] += 1;
-                }
+            let current = mult[index];
+            for slot in (index + 1)..(index + 1 + tot) {
+                mult[slot] += current;
             }
         }
     }
