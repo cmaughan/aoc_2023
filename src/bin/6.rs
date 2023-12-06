df79554abf072a4d7d6223bdd7d227a2ba3277d5
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2023-12-06 12:06:50 +0000

    optimal

--- src/bin/6.rs
+++ src/bin/6.rs
@@ -1,21 +1,16 @@
-
 fn run_trial(nums: Vec<u64>) -> u64 {
     let num_trials = nums.len() / 2;
     let mut tot = 1;
     for boat in 0..num_trials {
-        let win_distance = nums[boat + num_trials];
-        let total_time = nums[boat];
-        
-        let mut ways: u64 = 0;
-        for time in 0..total_time {
-            let speed = time;
-            let travel_time = total_time - time;
-            let distance = travel_time * speed;
-            if distance > win_distance {
-                ways += 1;
-            }
-        }
-        tot *= ways;
+        let win_distance = nums[boat + num_trials] as f64;
+        let total_time = nums[boat] as f64;
+
+        let mut end = (total_time - (total_time * total_time - 4.0 * win_distance).sqrt()) / -2.0;
+        let mut beg = (total_time + (total_time * total_time - 4.0 * win_distance).sqrt()) / -2.0;
+
+        if beg == beg.ceil() { beg += 1.0; }
+        if end == end.ceil() { end -= 1.0; }
+        tot *= (end.floor() - beg.ceil()) as u64 + 1;
     }
     tot
 }
@@ -23,26 +18,20 @@ fn run_trial(nums: Vec<u64>) -> u64 {
 #[aoc::main(6)]
 fn main(input: &str) -> (usize, usize) {
 
-    let p1 = run_trial(input
-        .split_whitespace()
-        .filter_map(|d| d.parse::<u64>().ok())
-        .collect::<Vec<u64>>());
+    let p1 = run_trial(
+        input
+            .split_whitespace()
+            .filter_map(|d| d.parse::<u64>().ok())
+            .collect::<Vec<u64>>(),
+    );
 
     let p2_chars = input
         .lines()
-        .map(|s| 
-            s
-                .chars()
-                .filter(|c| {
-                    c.is_digit(10)
-                })
-                .collect::<String>()
-        )
+        .map(|s| s.chars().filter(|c| c.is_digit(10)).collect::<String>())
         .map(|a| a.parse::<u64>().unwrap())
         .collect::<Vec<_>>();
     let p2 = run_trial(p2_chars);
 
-
     //503424, 32607562
-    (p1 as usize,  p2 as usize)
+    (p1 as usize, p2 as usize)
 }
